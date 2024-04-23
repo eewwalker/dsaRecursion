ea1a1efc286ad4fa91c21fe44a6c2e7c42bf6745
Emily Walker <ewalker415@gmail.com>
2024-04-23 12:31:49 -0700

    first two

---
+++ recursion/every-other/everyOther.ts
@@ -0,0 +1,21 @@
+/** everyOther: return a string with every other letter. */
+
+function everyOther(str: string): string {
+  if (str.length === 0) return '';
+
+  return str[0] + everyOther(str.slice(2));
+
+
+}
+
+/**
+ * ''
+ * hello => h + llo
+ * llo => l + o
+ *
+ *
+ *
+ */
+
+
+export { everyOther };
