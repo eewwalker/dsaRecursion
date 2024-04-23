6c71910006b5f7fd43d73e47460993c1e08bde27
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-04-23 16:37:16 -0700

    completed binarySearch problem

--- recursion/binary-search/binarySearch.test.ts
+++ recursion/binary-search/binarySearch.test.ts
@@ -1,7 +1,7 @@
 import { it, expect } from "vitest";
 import { binarySearch } from "./binarySearch";
 
-it("should find the index of a value in a sorted array", function () {
+it("should return true if value is in a sorted array", function () {
   expect(binarySearch([1, 2, 3, 4], 4)).toEqual(true);
   expect(binarySearch([1, 2], 1)).toEqual(true);
   expect(binarySearch([1, 2, 3, 4, 5, 6, 7], 6)).toEqual(true);
--- recursion/binary-search/binarySearch.ts
+++ recursion/binary-search/binarySearch.ts
@@ -2,7 +2,32 @@
  * return true if val is in array, false if not present). */
 
 function binarySearch(arr: number[], val: number, left = 0, right = arr.length): boolean {
-  return false;
+  if (left > right) return false;
+
+  const idx = left + Math.floor((right - left) / 2);
+
+  if (arr[idx] === val) return true;
+
+  if (arr[idx] > val) {
+    return binarySearch(
+      arr,
+      val,
+      left,
+      right = idx - 1);
+  } else {
+    return binarySearch(
+      arr,
+      val,
+      left = idx + 1,
+      right)
+  }
 }
 
 export { binarySearch };
+
+/**
+ *
+ *    arr: [1, 2], val: 11, left = 2, right = 2     idx = 1
+ * arr: [1, 2], val: 11, left = 0, right = 2     idx = 1
+
+ */
